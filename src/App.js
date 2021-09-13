b30274d5580cab27b21d93ab1084caf8ed1367d3
Jose Miguel Garcia <josemiguelgarcia@Joses-MacBook-Pro.local>
2021-09-13 17:11:33 -0500

    Adding under construcction page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import dancing_avocados from './assets/dance-avocado.gif'
+
+function App() {
+  return (
+    <div className="App">
+      Avocadox under construction
+      <div>
+        <img src={dancing_avocados} alt="Dancing avocado"></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
